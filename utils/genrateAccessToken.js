30800217435adedb75f2d34c5f3fd55a945065c2
parvinder-singh <parvinder49427@email.com>
2026-03-18 06:57:07 +0530

    Remove unused files and configurations: delete .gitignore, index.js, package.json, package-lock.json, and utility modules

--- utils/genrateAccessToken.js
+++
@@ -1,7 +0,0 @@
-import jwt from 'jsonwebtoken'
-
-const generateAccessToken = async(userId)=>{
-    const token =jwt.sign({id:userId},process.env.ACCESS_TOKEN_SECRET,{expiresIn:'5h'})
-    return token
-}
-export default generateAccessToken
--- utils/genrateOtp.js
+++
@@ -1,4 +0,0 @@
-const genrateOtp =()=>{
-    return Math.floor(Math.random()*900000) + 100000
-}
-export default genrateOtp
